3056e061c79af138871d2374ef639341d99aae75
SobiyaMoulvi <sobiyamoulvi299@gmail.com>
2021-08-05 17:43:18 +0530

    User Home Page component added

--- survey-expert-front-end/src/app/app-routing.module.ts
+++ survey-expert-front-end/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './AdminHomePage/admin-home/admin-home.component';
+import { UserHomeComponent } from './UserHomePage/user-home/user-home.component';
 
 const routes: Routes = [
   {path : '', redirectTo : 'adminHome',pathMatch : 'full'},
-  {path : 'adminHome', component : AdminHomeComponent}
+  {path : 'adminHome', component : AdminHomeComponent},
+  {path : 'userHome', component : UserHomeComponent}
 ];
 
 @NgModule({
--- survey-expert-front-end/src/app/app.module.ts
+++ survey-expert-front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './AdminHomePage/admin-home/admin-home.component';
+import { UserHomeComponent } from './UserHomePage/user-home/user-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHomeComponent
+    AdminHomeComponent,
+    UserHomeComponent
   ],
   imports: [
     BrowserModule,
