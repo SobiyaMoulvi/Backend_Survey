30fd4fa83b8b905a95604cdcb5ae3ba22f1d5895
SobiyaMoulvi <sobiyamoulvi299@gmail.com>
2021-08-06 21:14:37 +0530

    Created admin navbar and added register component

--- survey-expert-front-end/src/app/app-routing.module.ts
+++ survey-expert-front-end/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { PreviousSurveysComponent } from './Admin-ReviewPreviousSurvey/previous-surveys/previous-surveys.component';
 import { AdminHomeComponent } from './AdminHomePage/admin-home/admin-home.component';
 import { CreateNewSurveyComponent } from './createSurvey/create-new-survey/create-new-survey.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 import { UserHomeComponent } from './UserHomePage/user-home/user-home.component';
 
 const routes: Routes = [
-  {path : '', redirectTo : 'adminHome',pathMatch : 'full'},
+  {path : '', redirectTo : 'login-signup',pathMatch : 'full'},
   {path : 'adminHome', component : AdminHomeComponent},
   {path : 'userHome', component : UserHomeComponent},
   {path : 'previousSurvey', component : PreviousSurveysComponent},
-  {path : 'createSurvey', component : CreateNewSurveyComponent}
+  {path : 'createSurvey', component : CreateNewSurveyComponent},
+  {path : 'login-signup', component : RegisterUserComponent}
 ];
 
 @NgModule({
--- survey-expert-front-end/src/app/app.module.ts
+++ survey-expert-front-end/src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatIconModule,} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule,} from '@angular/material/button';
+import {MatCheckboxModule,} from '@angular/material/checkbox';
+import {MatFormFieldModule,} from '@angular/material/form-field';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './AdminHomePage/admin-home/admin-home.component';
 import { UserHomeComponent } from './UserHomePage/user-home/user-home.component';
 import { PreviousSurveysComponent } from './Admin-ReviewPreviousSurvey/previous-surveys/previous-surveys.component';
 import { CreateNewSurveyComponent } from './createSurvey/create-new-survey/create-new-survey.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +27,23 @@ import { CreateNewSurveyComponent } from './createSurvey/create-new-survey/creat
     AdminHomeComponent,
     UserHomeComponent,
     PreviousSurveysComponent,
-    CreateNewSurveyComponent
+    CreateNewSurveyComponent,
+    RegisterUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
